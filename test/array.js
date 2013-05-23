2484c16e6a9beb4886eb95d9bc9ec7f11400bab5
Tim Caswell <tim@creationix.com>
2013-05-23 16:51:58 -0500

    Add unit tests for array source

---
+++ test/array.js
@@ -0,0 +1,58 @@
+var array = require('../array.js');
+var test = require('tape');
+
+console.log(array);
+
+test("array should create a proper source stream", function (assert) {
+  var source = array([1,2,3]);
+  source(null, function (err, item) {
+    if (err) throw err;
+    assert.equal(item, 1);
+    source(null, function (err, item) {
+      if (err) throw err;
+      assert.equal(item, 2);
+      source(null, function (err, item) {
+        if (err) throw err;
+        assert.equal(item, 3);
+        source(null, function (err, item) {
+          if (err) throw err;
+          assert.equal(item, undefined);
+          assert.end();
+        });
+      });
+    });
+  });
+});
+
+test("array should create a proper async source stream", function (assert) {
+  var source = array.async([1,2,3]);
+  source(null, function (err, item) {
+    if (err) throw err;
+    assert.equal(item, 1);
+    source(null, function (err, item) {
+      if (err) throw err;
+      assert.equal(item, 2);
+      source(null, function (err, item) {
+        if (err) throw err;
+        assert.equal(item, 3);
+        source(null, function (err, item) {
+          if (err) throw err;
+          assert.equal(item, undefined);
+          assert.end();
+        });
+      });
+    });
+  });
+});
+
+test("array should respond to close", function (assert) {
+  var source = array.async([1,2,3]);
+  source(null, function (err, item) {
+    if (err) throw err;
+    assert.equal(item, 1);
+    source(true, function (err) {
+      if (err) throw err;
+      assert.end();
+    });
+  });
+});
